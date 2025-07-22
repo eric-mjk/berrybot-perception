dd42266ac8b22ca17672d7e5c6e20892dd554039
eric-mjk <eric.mjkim35@gmail.com>
2025-07-22 19:51:22 +0900

    Delete functions directory

--- functions/test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-url = r"C:\Users\ericm\Desktop\BerryBot\Tests_Tests\TEST1\NaiveTraining_Results2\runs\pose\yolov8n_strawberry_continued\weights\best.pt"
-
-path = Path(url)
-print(path.as_posix())
