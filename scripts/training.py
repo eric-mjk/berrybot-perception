6d1dadc3b181666a5b6e8e485923bcb7d2e7b8f2
eric-mjk <eric.mjkim35@gmail.com>
2025-07-22 23:35:17 +0900

    Minor edit (on training proces)

--- scripts/training.py
+++ scripts/training.py
@@ -16,22 +16,61 @@ print(ultralytics.__version__)
 
 import ultralytics
 import torch
-
-###############################################################################
 from ultralytics import YOLO
+
+# Target Directory
+path = "Strawberry Feature Points.v11-complete_dataset.yolov8/data.yaml"
+
+1 ###############################################################################
 model = YOLO("yolov8s-pose.yaml")  # build a new model from YAML
 
 model.train(
-    data='/content/drive/MyDrive/BerryBot/TESTS/4. Test4/masked_dataset.yolov8/data.yaml',
-    epochs=50,
+    data= path,
+    epochs=100,
     imgsz=640,
     batch=16,
     workers=2,
-    name='yolov8n_strawberry',
+    name='yolov8n_strawberry_8s',
+    project='C:/Users/user/Desktop/User_Eric/data',
 )
 
 
-# Target Directory
-path = "Strawberry Feature Points.v11-complete_dataset.yolov8"
+2 ###############################################################################
+model = YOLO("yolo11s-pose.yaml")  # build a new model from YAML
 
+model.train(
+    data= path,
+    epochs=100,
+    imgsz=640,
+    batch=16,
+    workers=2,
+    name='yolov8n_strawberry_11s',
+    project='C:/Users/user/Desktop/User_Eric/data',
+)
+
+3 ###############################################################################
+model = YOLO("yolov8s-pose.pt")  # build a new model from YAML
+
+model.train(
+    data= path,
+    epochs=100,
+    imgsz=640,
+    batch=16,
+    workers=2,
+    name='yolov8n_strawberry_pretrained8s',
+    project='C:/Users/user/Desktop/User_Eric/data',
+)
+
+4 ###############################################################################
+model = YOLO("yolo11s-pose.pt")  # build a new model from YAML
+
+model.train(
+    data= path,
+    epochs=100,
+    imgsz=640,
+    batch=16,
+    workers=2,
+    name='yolov8n_strawberry_pretrained8s',
+    project='C:/Users/user/Desktop/User_Eric/data',
+)
 
